4a1cfed6c0733bcdc5f1f997a051a0f74d8e7a95
unknown <butronoscar6@gmail.com>
2024-12-19 21:00:38 -0500

    first commit

---
+++ java.js
@@ -0,0 +1,36 @@
+document.getElementById("registrationForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const fullName = document.getElementById("fullName").value;
+    const age = document.getElementById("age").value;
+    const nationality = document.getElementById("nationality").value;
+    const correo = document.getElementById("correo").value;
+    const numero = document.getElementById("numero").value;
+
+    if (age < 18) {
+        alert("Debe ser mayor de 18 años para inscribirse.");
+        return;
+    }
+    if (nationality !== "Peruano") {
+        alert("Solo los peruanos pueden inscribirse en esta conferencia.");
+        return;
+    }
+
+    const tableBody = document.getElementById("tableBody");
+    const newRow = document.createElement("tr");
+
+    newRow.innerHTML = `
+        <td>${fullName}</td>
+        <td>${age}</td>
+        <td>${nationality}</td>
+        <td>${correo}</td>
+        <td>${numero}</td>
+        
+    `;
+
+    tableBody.appendChild(newRow);
+
+    document.getElementById("confirmationTable").style.display = "block";
+
+    document.getElementById("registrationForm").reset();
+});
